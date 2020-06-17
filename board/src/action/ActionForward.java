7996dafbfd87d1cb87a38e210292e4b0dcf9dad2
joyui <seclusionor@gmail.com>
2020-06-17 17:22:14 +0900

    보드

---
+++ board/src/action/ActionForward.java
@@ -0,0 +1,14 @@
+package action;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class ActionForward {
+	private String path;
+	private boolean redirect;
+}
